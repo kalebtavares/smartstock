6137ef5bbf69c4393fa907b9735a4f83399fc4f4
kalebtavaressilva <kalebtavares@icloud.com>
2026-03-12 14:13:10 -0400

    feat(usuario): Adiciona entidade Usuario e configura JPA
    Adiciona a entidade Usuario com campos id, nome, email e senha para persistência de dados.
    Configura o pom.xml para incluir a dependência spring-boot-starter-data-jpa e postgresql.

---
+++ src/main/java/com/smartstock/smartstock/infrastructure/entity/Usuario.java
@@ -0,0 +1,27 @@
+package com.smartstock.smartstock.infrastructure.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "usuario")
+public class Usuario{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "nome", length = 100)
+    private String nome;
+    @Column(name = "email", length = 100)
+    private String email;
+    @Column(name = "senha", length = 10)
+    private String senha;
+
+}
