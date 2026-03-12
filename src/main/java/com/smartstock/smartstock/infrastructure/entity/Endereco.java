321220cfad001dfa5764304630b58fe90db136f0
kalebtavaressilva <kalebtavares@icloud.com>
2026-03-12 15:31:30 -0400

    feat(entity): Adicionar campos à entidade Endereco
    Adiciona campos como rua, numero, complemento, cidade, estado e cep à entidade Endereco.
    Isso permite um armazenamento mais completo dos dados de endereço.

--- src/main/java/com/smartstock/smartstock/infrastructure/entity/Endereco.java
+++ src/main/java/com/smartstock/smartstock/infrastructure/entity/Endereco.java
@@ -1,8 +1,7 @@
 package com.smartstock.smartstock.infrastructure.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,4 +14,21 @@ import lombok.Setter;
 @Entity
 @Table(name = "endereco")
 public class Endereco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "rua")
+    private String rua;
+    @Column(name = "numero")
+    private Long numero;
+    @Column (name = "complemento", length = 10)
+    private String complemento;
+    @Column (name = "cidade", length = 100)
+    private String cidade;
+    @Column (name = "estado", length = 2)
+    private String estado;
+    @Column (name = "cep", length = 9)
+    private String cep;
+
+
 }
