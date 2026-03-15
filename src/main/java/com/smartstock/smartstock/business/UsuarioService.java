689b9fe4c29c4f8ebe2d45603c983476a6c03a4b
kaleb-tavares <kalebtavares@icloud.com>
2026-03-15 12:12:30 -0400

    feat(usuario): Adiciona validação de email e exceção de conflito
    Adiciona ConflictException para tratar conflitos de dados.
    Implementa validação de email existente no UsuarioService.
    Lança ConflictException quando o email já está cadastrado.

--- src/main/java/com/smartstock/smartstock/business/UsuarioService.java
+++ src/main/java/com/smartstock/smartstock/business/UsuarioService.java
@@ -1,6 +1,7 @@
 package com.smartstock.smartstock.business;
 
 import com.smartstock.smartstock.infrastructure.entity.Usuario;
+import com.smartstock.smartstock.infrastructure.exceptions.ConflictException;
 import com.smartstock.smartstock.infrastructure.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,14 +15,28 @@ public class UsuarioService {
     public Usuario salvaUsuario(Usuario usuario) {
 
         try {
-
+            emailExiste(usuario.getEmail());
             return usuarioRepository.save(usuario);
-
-        } catch () {
+        } catch (ConflictException e) {
+            throw new ConflictException("Email já cadastrado", e.getCause());
 
         }
     }
-    public boolean verificaEmailExistente(String email){
-        return usuarioRepository.existsByEmail(email);
+
+        public void emailExiste (String email){
+            try {
+                boolean existe = verificaEmailExistente(email);
+
+                if (existe) {
+                    throw new ConflictException("Email já adastrado" + email);
+                }
+
+            } catch (ConflictException e) {
+                throw new ConflictException("Email já cadastrado", e.getCause());
+            }
+        }
+
+        public boolean verificaEmailExistente (String email){
+            return usuarioRepository.existsByEmail(email);
+        }
     }
-}
